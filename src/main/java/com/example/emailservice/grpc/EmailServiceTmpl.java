ab1b258a04694e3d1cefbb88bbba1927ea934dff
august <ghrp8277@naver.com>
2024-07-18 02:11:20 +0900

    1

--- src/main/java/com/example/emailservice/grpc/EmailServiceTmpl.java
+++ src/main/java/com/example/emailservice/grpc/EmailServiceTmpl.java
@@ -1,5 +1,6 @@
 package com.example.emailservice.grpc;
 
+import com.example.emailservice.annotation.GrpcExceptionHandler;
 import com.example.emailservice.service.EmailService;
 import com.example.emailservice.util.GrpcResponseHelper;
 import com.example.grpc.*;
@@ -17,12 +18,14 @@ public class EmailServiceTmpl extends EmailServiceGrpc.EmailServiceImplBase {
     private GrpcResponseHelper grpcResponseHelper;
 
     @Override
+    @GrpcExceptionHandler
     public void emailSend(EmailSendRequest request, StreamObserver<Response> responseObserver) {
         String message = emailService.emailCodeInRedisWithTTL(request.getUserId(), request.getEmail());
         grpcResponseHelper.sendJsonResponse("message", message, responseObserver);
     }
 
     @Override
+    @GrpcExceptionHandler
     public void verifyEmailCode(VerifyEmailCodeRequest request, StreamObserver<Response> responseObserver) {
         boolean isValid = emailService.verifyEmailCode(request.getUserId(), request.getEmail(), request.getCode());
         grpcResponseHelper.sendJsonResponse("isValid", isValid, responseObserver);
