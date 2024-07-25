22008f7aacc5bdde75e223b9b244bed46adce136
august <ghrp8277@naver.com>
2024-07-25 09:22:52 +0900

    env 설정

--- src/main/java/com/example/emailservice/EmailServiceApplication.java
+++ src/main/java/com/example/emailservice/EmailServiceApplication.java
@@ -2,11 +2,18 @@ package com.example.emailservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class EmailServiceApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+
+        dotenv.entries().forEach(entry -> {
+            System.setProperty(entry.getKey(), entry.getValue());
+        });
+
         SpringApplication.run(EmailServiceApplication.class, args);
     }
 
